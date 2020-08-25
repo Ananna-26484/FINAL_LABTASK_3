300bcda0adb1bf1d01fc07423964e023b21d7fd6
Sanjida Akter Ananna <14-26484-2@student.aiub.edu>
2020-08-25 10:03:43 +0600

    validation up to email, full was not done, class is over.

--- check.php
+++ check.php
@@ -16,4 +16,21 @@ if(isset($_GET['submit']))
 	    
 }
 
+if(isset($_GET['submit']))
+{
+	
+    $email=$_GET['email'];
+	if(strlen($email)>0)
+	{
+		echo"Email:  ";
+        echo $email ;      
+		
+	}
+	else
+	{
+		echo "email can not be empty";
+    }   
+	    
+}
+
 ?>
--- script.js
+++ script.js
@@ -3,11 +3,16 @@
 function validate(){
 
 	var username = document.getElementById('username').value;
+	var email = document.getElementById('email').value;
 	
 	if(username == ""){
 		document.getElementById('userMsg').innerHTML = "username can't left empty";
 		return false;
 	}	
+	else if(email == ""){
+		document.getElementById('userMsg').innerHTML = "* email can't left empty";
+		return false;
+	}
 	else{
 		return true;
 	}
@@ -16,11 +21,16 @@ function validate(){
 
 function remover(){
 	document.getElementById('userMsg').innerHTML = "";
+	document.getElementById('emailMsg').innerHTML = "";
 }
 
 function blurmsg(){
 
-	if(document.getElementById('username').value == ""){
-		document.getElementById('userMsg').innerHTML = "* this field is required!";
+	if(document.getElementById('username').value == "" ){
+		document.getElementById('userMsg').innerHTML = "* this field is required!";		
 	}
+	if( document.getElementById('email').value == ""){		
+		document.getElementById('emailMsg').innerHTML = "* this field is required!";
+	}
+	
 }
